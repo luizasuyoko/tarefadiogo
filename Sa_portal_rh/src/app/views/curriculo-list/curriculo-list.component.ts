c9f7261fea8e9fa816c381dd3d6dfa869eaadfb5
luizasuyoko <luizasuemiyokomizo@outlook.com>
2025-06-24 11:28:29 -0300

    24.06.2025

--- Sa_portal_rh/src/app/views/curriculo-list/curriculo-list.component.ts
+++ Sa_portal_rh/src/app/views/curriculo-list/curriculo-list.component.ts
@@ -9,50 +9,21 @@ import { curriculosService } from 'src/app/services/curriculo.service';
 })
 
 export class CurriculosListComponent implements OnInit{
-  termoBusca: string = '';
-  resultados: Curriculo[] = [];
-  buscou: boolean = false;
+   curriculos: Curriculo[] = [];
+  filtroId = '';
 
-  private todosCurriculos: Curriculo[] = [];
-
-  constructor(private curriculoService: curriculosService) {}
+  constructor(private curriculosService: curriculosService) {}
 
   ngOnInit(): void {
-    // Carrega todos os currículos uma vez
-    this.curriculoService.getCurriculo().subscribe(
-      (dados) => {
-        this.todosCurriculos = dados.map((c) => Curriculo.fromMap(c));
-      },
-      () => {
-        alert('Erro ao carregar currículos');
-      }
-    );
+    this.curriculosService.getCurriculo().subscribe({
+      next: (dados) => (this.curriculos = dados),
+      error: (erro) => console.error('Erro ao buscar currículos:', erro),
+    });
   }
 
-  buscar(): void {
-    this.resultados = [];
-    this.buscou = true;
-
-    const termo = this.termoBusca.trim().toLowerCase();
-
-    if (!termo) return;
-
-    // Verifica se é número (ID)
-    if (!isNaN(Number(termo))) {
-      const id = Number(termo);
-      this.curriculoService.getCurriculoById(id).subscribe(
-        (curriculo) => {
-          this.resultados = [Curriculo.fromMap(curriculo)];
-        },
-        () => {
-          this.resultados = [];
-        }
-      );
-    } else {
-      // Busca por nome (case insensitive)
-      this.resultados = this.todosCurriculos.filter((curriculo) =>
-        curriculo.nome.toLowerCase().includes(termo)
-      );
-    }
-  }
-}
+  get curriculosFiltrados(): Curriculo[] {
+    return this.curriculos.filter(c => {
+      const idOk = this.filtroId === '' || c.id.toString().includes(this.filtroId);
+      return idOk;
+    });
+  }}
